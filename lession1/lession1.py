c2a3c775d6d327b59727e9a4625d3c89a5d6a6f5
Nguyen Duc Hieu <hieund@geniee.co.jp>
2024-09-21 21:57:23 +0700

    Init Project + Lession 1

---
+++ lession1/lession1.py
@@ -0,0 +1,100 @@
+# input -> xử lý -> đầu ra
+
+
+# chương trình hello world
+
+
+# Kiểu dữ liệu
+
+"""
+
+Chú thích -> Không ảnh hưởng đến code
+
+7 kiểu dữ liệu
+
++ Số nguyên -> integer: VD: 1 2 3 4 5 6 -> viết tắt: int
+
++ Số thực -> Float: 0.1 1.2 10.1 -> float
+
++ String -> chuỗi -> có thể là chữ hoặc số phải nằm trong dấu ngoặc kép hoặc đơn -> str
+
+VD: "Hello World", '100' -> string
+
++ Boolen: True, False -> bool
+
+"""
+
+"""
+Toán tử:
+
++
+
+-
+
+*
+
+/
+
+"""
+
+x = 10
+y = 20
+
+# Hello = 100
+# world = 200
+
+# biến số
+
+# quy tắc đặt tên biến
+
+# Không đặt tên biến có ký tự đặc biệt # @ ! % &
+# Không đặt tên biến có chứa ký tự số nằm ở đầu tiên
+
+
+"""
+Tính diện tích hình chữ nhật biết
+cạnh dài x = 50
+cạnh ngặn y = 30
+
+sử dụng print và in ra đáp: Diện tích 1500 HCN
+"""
+
+# seperate invertion string
+
+# print(f"Diện tích HCN {x * y} siêu to khổng lồ")
+
+# ép kiểu
+
+# string -> integer
+
+# print(10 % 3)
+
+# print(10 // 3)
+
+
+
+"""
+
+Áp dụng định lý pytago
+TÍnh cạnh huyền của tam giác
+
+tạo input nhập 2 cạnh góc vuông
+
+"""
+
+# z = "Hello"
+
+# y = "World"
+
+# print(z * 4)
+
+
+x = int(input("Nhập cạnh 1: "))
+y = int(input("Nhập cạnh 2: "))
+
+z = (x ** 2 + y ** 2) ** (1/2)
+
+print("x**2 + y**2 = ", z)
+
+
+
