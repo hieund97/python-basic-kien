44bca9bbdc499e3dd085feb4ed77204f57b1146d
Nguyen Duc Hieu <hieund@geniee.co.jp>
2024-10-08 20:36:12 +0700

    commit lession2

---
+++ lession2/lession2.py
@@ -0,0 +1,100 @@
+"""
+
+Int
+Float
+String
+Boolen
+List
+Tuple
+Dictionary
+
+
+%
+
+//
+
+**
+"""
+
+
+"Xin chào Gemini hôm nay là thứ 3"
+
+a = [8,9,10,11,12,13,14,15,16,17,18,19,20,1,2,3,4,5,6,7,14,15,16,17,18,19,18,19,20,1,2,3]
+# print(len(a))
+
+# a.append(100)
+
+# a.insert(index, value)
+# a.insert(3, "hello")
+
+# xoá 1 phần tử trong list
+# a.pop()
+
+
+# xoá toàn bộ phần tử trong list
+# a.clear()
+
+
+# remove xoá 1 phần tử trong list
+# a.remove("hello")
+
+# print(max(a))
+# print(min(a))
+# print(sum(a))
+
+a.sort(reverse=True) # thay đổi list gốc
+
+print(sorted(a, reverse=True)) # không thay đổi list gốc
+
+
+
+"""
+
+
+
+
+
+"""
+
+
+
+"""
+List
+    index
+    List 2 chiều
+    Phương thức của list
+For
+
+
+
+"""
+
+# 2 trường hợp:
+"""
+* step > 0
+mặc định:
+start = 0
+end = n -1
+
+
+* step < 0
+mặc định:
+start = -1
+end = -n
+"""
+
+
+"""
+Tính số phần tử của list
+
+len()
+
+
+
+a = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+len(a) = 20
+
+
+"""
+
+
