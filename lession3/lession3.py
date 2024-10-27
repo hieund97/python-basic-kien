fae6cf3ba8f76b149fbab71c012e102bff5123e9
Nguyen Duc Hieu <hieund@geniee.co.jp>
2024-10-27 21:49:22 +0700

    add lession3

---
+++ lession3/lession3.py
@@ -0,0 +1,39 @@
+list_menu = {
+    "apple": 100000,
+    "banana": 200000,
+    "orange": 300000,
+    "mango": 400000,
+    "grape": 500000,
+    "pineapple": 600000,
+    "cherry": 700000,
+    "watermelon": 800000,
+}
+
+"""
+viết 1 chương trình có 3 chức năng
+1. Hiện thị menu:
+    + Apple - Giá: 100000 đ/ kg
+    + Banana - Giá: 200000 đ/ kg
+    + Orange - Giá: 300000 đ/ kg
+    
+2. Mua hàng:
+    + Nhap vao ten san pham muon mua
+    + kiểm tra sản phẩm có tồn tại hay không
+    + Nếu tồn tại nhập số lượng muốn mua (kg)
+    + Tính ra giá tiền cần trả
+        Hoá đơn: Apple - 3kg - Tổng: 300000đ
+3. Thoát
+"""
+
+while True:
+    choice = int(input("Nhập chức năng: "))
+    if choice == 1:
+        # gọi hafmg 1
+        pass
+    elif choice == 2:
+        # gọi ham 2
+        pass
+    elif choice == 3:
+        break
+
+    
